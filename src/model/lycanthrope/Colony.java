3ad36c60fae3900e3fe3377db3df48d532b2919b
François <francois.dargentolle@etu.univ-amu.fr>
2025-12-10 10:39:23 +0100

    New : Javadoc for the pack and for the colony

--- src/model/lycanthrope/Colony.java
+++ src/model/lycanthrope/Colony.java
@@ -5,6 +5,15 @@ import model.character.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class of the colony of Lycanthrope objects
+ * 
+ * <p> This class is used to initialize the methods used in the colony of lycanthrope</p>
+ * @author Benhafessa Alexandre
+ * @author Dargentolle François
+ * @author Edelstein William
+ * @author Griguer Nathan
+ */
 public class Colony {
 	private List<Pack> packs;
 	private List<FantasticCreaturesLycanthropes> solitaries; // j'ai un doute sur celle la
@@ -13,17 +22,38 @@ public class Colony {
         this.packs = new ArrayList<>();
         this.solitaries = new ArrayList<>();
     }
+	
+	/**
+	 * Method that add a pack to the list of pack
+	 * @param p the pack of lycanthrope
+	 * @return void
+	 */
 	public void addPack(Pack p) {
         this.packs.add(p);
     }
+	
+	/**
+	 * Method that manage the lycanthrope in solitary state
+	 * @return void
+	 */
 	public void manageSolitaries() {
 		//TODO
 		//Il s'agit du maitre de zoo fantastique qui peut bouger les solitaires (chef de clan)
     }
+	
+	/**
+	 * Getter for the list of pack
+	 * @return packs
+	 */
 	public List<Pack> getPacks() {
         return packs;
     }
 
+	/**
+	 * Method that age the members of a pack
+	 * @param years
+	 * @return void 
+	 */
     public void fastForwardTime(int years) {
         // ToDo : avancer le temps dans la colinie pour faire vieillir les lycanthropes , à voir avec la simulation du controleur
     }
--- src/model/lycanthrope/Pack.java
+++ src/model/lycanthrope/Pack.java
@@ -5,7 +5,15 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import model.character.*;
-
+/**
+ * Class of the pack of Lycanthrope objects
+ * 
+ * <p> This class is used to initialize the methods used in the pack of lycanthrope</p>
+ * @author Benhafessa Alexandre
+ * @author Dargentolle François
+ * @author Edelstein William
+ * @author Griguer Nathan
+ */
 public class Pack {
 	private List<FantasticCreaturesLycanthropes> members;
 	private FantasticCreaturesLycanthropes alphaMale;
@@ -15,16 +23,35 @@ public class Pack {
 	public Pack() {
 		this.members = new ArrayList<FantasticCreaturesLycanthropes>();
 	}
+	
+	/**
+	 * Method that add an object lycanthrope to the list of members of a pack
+	 * @param wolf
+	 * @return void
+	 */
 	public void addMember (FantasticCreaturesLycanthropes wolf) {
 		members.add(wolf);
 		//wolf.setPack(this);
 		recalculateHierarchy();
 	}
+	
+	/**
+	 * Method that remove an object lycanthrope to the list of members of a pack and recalculate the hierarchy
+	 * @param wolf
+	 * @return void
+	 */
 	public void removeMember (FantasticCreaturesLycanthropes wolf) {
 		members.remove(wolf);
 		//wolf.setPack(this);
 		recalculateHierarchy();
 	}
+	
+	/**
+	 * Method that re-arrange the hierarchy of the pack (mostly the alpha couple)
+	 * <p> This methode will verify if a male and female are worthy of becoming alpha and if they do, then their rank will change in consequence
+	 *  only if there isn't another alpha from the same gender as them </p>
+	 * @return void
+	 */
 	public void recalculateHierarchy() {
 		for (FantasticCreaturesLycanthropes member : members) {
 			if(member.getLevel() < alphaMale.getLevel() && !member.equals(alphaMale) && member.isMale()) {
@@ -36,6 +63,13 @@ public class Pack {
 		}
 		
 	}
+	
+	/**
+	 * Method that will make baby out of the alpha couple
+	 * <p> This method will make the couple reproduce a random number of cub between 1 and 7. The method also assign the attribute of the cub while 
+     * the rank of everyone in the pack</p>
+     * @return void
+	 */
 	public void reproduce() {
 		if (alphaMale != null && alphaFemale != null) {
 			int litterSize = 1 + (int)(Math.random()*7);
@@ -64,6 +98,10 @@ public class Pack {
 		}
 	}
 	
+	/**
+	 * Method that will sort the lycanthrope thanks to their rank
+	 * @return result the list of lycanthrope sorted by rank
+	 */
 	public List<FantasticCreaturesLycanthropes> getMembersSortedByRank(){
 		// a tester
 		List<FantasticCreaturesLycanthropes> result = new ArrayList<>();
@@ -72,21 +110,44 @@ public class Pack {
 		return result;
 	}
 	
+	/**
+	 * Getter of the list of lycanthrope
+	 * @return members
+	 */
 	public List<FantasticCreaturesLycanthropes> getMembers(){
 		return members;
 	}
 	
+	/**
+	 * Setter of the alpha male
+	 * @param male
+	 * @return void
+	 */
 	public void setAlphaMale(FantasticCreaturesLycanthropes male) {
 		this.alphaMale = male;
 	}
 	
+	/**
+	 * Setter of the alpha female
+	 * @param female
+	 * @return void
+	 */
 	public void setAlphaFemale(FantasticCreaturesLycanthropes female) {
 		this.alphaFemale = female;
 	}
 	
+	/**
+	 * Getter of the alpha male
+	 * @return alphaMale
+	 */
 	public FantasticCreaturesLycanthropes getAlphaMale() {
 		return this.alphaMale;
 	}
+	
+	/**
+	 * Getter of the alpha female
+	 * @return alphaFemale
+	 */
 	public FantasticCreaturesLycanthropes getAlphaFemale() {
 		return this.alphaFemale;
 	}
